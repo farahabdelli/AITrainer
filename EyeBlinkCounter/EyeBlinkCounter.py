1ba9808932fb01e897ce4abe58d4048126e7898e
farahabdelli <farah.abdelli@esprit.tn>
2022-09-29 10:42:47 +0200

    eyeblink

--- EyeBlinkCounter/EyeBlinkCounter.py
+++ EyeBlinkCounter/EyeBlinkCounter.py
@@ -3,38 +3,39 @@ import cvzone
 from cvzone.FaceMeshModule import FaceMeshDetector
 from cvzone.PlotModule import LivePlot
 
-cap = cv2.VideoCapture('Video.mp4')
-detector = FaceMeshDetector(maxFaces=1)
 plotY = LivePlot(640, 360, [20, 50], invert=True)
 
-idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243,252,253,254,255,256,384,385,386,388,263,359]
 ratioList = []
 blinkCounter = 0
 counter = 0
 color = (255, 0, 255)
 
+
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector(maxFaces=1)
 while True:
-
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
     success, img = cap.read()
-    img, faces = detector.findFaceMesh(img, draw=False)
+    img = cv2.flip(img,1)
+    img, faces = detector.findFaceMesh(img,draw=False)
+
+
 
     if faces:
         face = faces[0]
-        for id in idList:
-            cv2.circle(img, face[id], 5,color, cv2.FILLED)
+        #for id in idList:
+           # cv2.circle(img, face[id], 5,color, cv2.FILLED)
 
         leftUp = face[159]
         leftDown = face[23]
         leftLeft = face[130]
         leftRight = face[243]
+   
         lenghtVer, _ = detector.findDistance(leftUp, leftDown)
         lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
 
-        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
-        cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
+       # cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+        #cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
 
         ratio = int((lenghtVer / lenghtHor) * 100)
         ratioList.append(ratio)
@@ -62,5 +63,9 @@ while True:
         img = cv2.resize(img, (640, 360))
         imgStack = cvzone.stackImages([img, img], 2, 1)
 
-    cv2.imshow("Image", imgStack)
-    cv2.waitKey(25)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+      break
+    
+cap.release()
+cv2.destroyAllWindows()
